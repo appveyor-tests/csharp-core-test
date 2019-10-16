468c67e327a7f004c7b5d734395e2897c7cb179c
Feodor Fitsner <feodor@appveyor.com>
2019-10-16 10:59:38 -0700

    Initial check-in

---
+++ MyCSharpCoreTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MyCSharpCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var json = JsonConvert.SerializeObject(new { a = 1 });
+            Console.WriteLine(json);
+        }
+    }
+}
